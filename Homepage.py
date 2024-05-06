4ea20e821c163b6449da3d259856fdeb371b4748
Jukainite <114989558+Jukainite@users.noreply.github.com>
2024-05-06 10:57:06 +0700

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -293,8 +293,8 @@ def show_palmistry_page():
             self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
             self.conv5 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
             self.pool = nn.MaxPool2d(2, 2)
-            # self.fc1 = nn.Linear(128 * 4 * 4, 128)
-            # self.fc2 = nn.Linear(128, len(classes))
+            self.fc1 = nn.Linear(128 * 4 * 4, 128)
+            self.fc2 = nn.Linear(128, len(classes))
             self.fc1 = nn.Linear(512 * 4 * 4, 2048)  # Adjust input size here
             self.fc2 = nn.Linear(2048, len(classes))  # Adjust output size here
 
@@ -304,8 +304,8 @@ def show_palmistry_page():
             x = self.pool(nn.functional.relu(self.conv3(x)))
             x = self.pool(nn.functional.relu(self.conv4(x)))
             x = self.pool(nn.functional.relu(self.conv5(x)))
-            # x = x.view(-1, 128 * 4 * 4)
-            x = x.view(-1, 512 * 4 * 4)
+            x = x.view(-1, 128 * 4 * 4)
+            # x = x.view(-1, 512 * 4 * 4)
             x = nn.functional.relu(self.fc1(x))
             x = self.fc2(x)
             return x

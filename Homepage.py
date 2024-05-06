09fb4050b21e8bf2f07a0152d77118db28ecbf14
Jukainite <114989558+Jukainite@users.noreply.github.com>
2024-05-06 14:03:48 +0700

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -285,27 +285,27 @@ def show_palmistry_page():
     classes = ['Hình cung', 'Vòng tròn hướng tâm', 'Vòng lặp Ulnar', 'Vòm lều', 'Vòng xoáy']
 
     class FingerprintCNN(nn.Module):
-    def __init__(self):
-        super(FingerprintCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.conv5 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(128 * 4 * 4, 128)
-        self.fc2 = nn.Linear(128, len(classes))
-
-    def forward(self, x):
-        x = self.pool(nn.functional.relu(self.conv1(x)))
-        x = self.pool(nn.functional.relu(self.conv2(x)))
-        x = self.pool(nn.functional.relu(self.conv3(x)))
-        x = self.pool(nn.functional.relu(self.conv4(x)))
-        x = self.pool(nn.functional.relu(self.conv5(x)))
-        x = x.view(-1, 128 * 4 * 4)
-        x = nn.functional.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+        def __init__(self):
+            super(FingerprintCNN, self).__init__()
+            self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+            self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+            self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+            self.conv4 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+            self.conv5 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+            self.pool = nn.MaxPool2d(2, 2)
+            self.fc1 = nn.Linear(128 * 4 * 4, 128)
+            self.fc2 = nn.Linear(128, len(classes))
+    
+        def forward(self, x):
+            x = self.pool(nn.functional.relu(self.conv1(x)))
+            x = self.pool(nn.functional.relu(self.conv2(x)))
+            x = self.pool(nn.functional.relu(self.conv3(x)))
+            x = self.pool(nn.functional.relu(self.conv4(x)))
+            x = self.pool(nn.functional.relu(self.conv5(x)))
+            x = x.view(-1, 128 * 4 * 4)
+            x = nn.functional.relu(self.fc1(x))
+            x = self.fc2(x)
+            return x
     # Load the trained model
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     

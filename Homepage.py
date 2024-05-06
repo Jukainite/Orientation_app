4bbae2b6873d981125de6d841bb3c0450159b3d4
Jukainite <114989558+Jukainite@users.noreply.github.com>
2024-05-06 14:05:25 +0700

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -345,7 +345,7 @@ def show_palmistry_page():
     
     
     # Function to predict label for input image
-    def predict_label(image_path):
+    def predict_label(img):
         # img = cv2.imread(image_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to RGB
         img = cv2.resize(img, (128, 128))  # Resize the image to 128x128
